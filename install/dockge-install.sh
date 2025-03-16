36a2d4f0cf6191b89bb5db3c756b9ce157030842
fishboro <57053892+fishboro@users.noreply.github.com>
2025-03-16 18:05:08 -0400

    DockgeLXC now deploys Socket Proxy with Dockge

--- install/dockge-install.sh
+++ install/dockge-install.sh
@@ -43,41 +43,10 @@ msg_ok "Installed Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
 
 msg_info "Installing Dockge"
 mkdir -p /opt/{dockge,stacks}
-wget -q -O /opt/dockge/compose.yaml https://raw.githubusercontent.com/louislam/dockge/master/compose.yaml
+wget -q -O /opt/dockge/compose.yaml https://raw.githubusercontent.com/fishboro/Proxmox-Helpers/main/install/dockge-compose.yaml
 cd /opt/dockge
 $STD docker compose up -d
 msg_ok "Installed Dockge"
-
-read -r -p "Would you like to add Immich? <y/N> " prompt
-if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
-  msg_info "Adding Immich compose.yaml"
-  mkdir -p /opt/stacks/immich
-  wget -q -O /opt/stacks/immich/compose.yaml https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml
-  wget -q -O /opt/stacks/immich/.env https://github.com/immich-app/immich/releases/latest/download/example.env
-  #wget -q -O /opt/stacks/immich/hwaccel.ml.yml https://github.com/immich-app/immich/releases/latest/download/hwaccel.ml.yml
-  msg_ok "Added Immich compose.yaml"
-fi
-
-read -r -p "Would you like to add Home Assistant? <y/N> " prompt
-if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
-  msg_info "Adding Home Assistant compose.yaml"
-  mkdir -p /opt/stacks/homeassistant
-  cat <<EOF >/opt/stacks/homeassistant/compose.yaml
-version: "3"
-services:
-  homeassistant:
-    container_name: homeassistant
-    image: ghcr.io/home-assistant/home-assistant:stable
-    volumes:
-      - ./config:/config
-      - /etc/localtime:/etc/localtime:ro
-      - /run/dbus:/run/dbus:ro
-    restart: unless-stopped
-    privileged: true
-    network_mode: host
-EOF
-msg_ok "Added Home Assistant compose.yaml"
-fi
 
 motd_ssh
 customize
